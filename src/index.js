4f28ba842e0a0589606cd978b497ea27d0b79432
Muffin135 <lionelqwork@gmail.com>
2020-08-19 15:11:59 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+document.getElementById("body").innerHTML = `
+<div class="home">
+      <div class="header">
+        <div class="logo">
+          <img
+            id="logo_img"
+            src="https://www.outsidepride.com/images/products/detail/gardenflower/birdofparadise.jpg"
+          />
+        </div>
+        <h1 class="home_title">DARK RIDER INDUSTRIES</h1>
+        <!--
+        <p class="home_byline">
+          Come into my palace, girl. Everything you've ever wanted, <wbr />even
+          if you don't want to stay.
+        </p>
+        
+      </div>
+      <div class="menu">
+        <a class="menu-item">About</a>
+        <a class="menu-item">News</a>
+        <a class="menu-item">Events</a>
+        <a class="menu-item">Store</a>
+        <a class="menu-item">Artists</a>
+        <a class="menu-item">Catalogue</a>
+      </div>
+    </div>
+`;
